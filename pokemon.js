00662a871499b3b3b05213c2101c629ead4d8d74
Justin Boker <bokerjustin@gmail.com>
2022-10-12 14:48:02 -0500

    added comments

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,4 @@
+//Establishes the Pokemon class
 class Pokemon {
     constructor(name, level, type) {
         this.name = name;
@@ -6,6 +7,7 @@ class Pokemon {
     }
 }
 
+//Establishes the Trainer class - also includes a method to add Pokemon to that specific Trainer
 class Trainer {
     constructor(id, name) {
         this.id = id;
@@ -18,25 +20,33 @@ class Trainer {
     }
 }
 
+//Creates an empty trainers array and sets the trainderId to 0
 let trainers = [];
 let trainerId = 0;
 
+//Activating the onClick function. This takes the given trainer's name and creates a new instance of Trainer and runs the getValue function.
+    //Then it activates the drawDOM function. Finally, it clears the Trainer's Name input field on the webpage.
 onClick('new-trainer', () => {
     trainers.push(new Trainer(trainerId++, getValue('new-trainer-name')));
     drawDOM();
     document.getElementById('new-trainer-name').value = '';
 });
 
+//This function takes an ide and an action as parameters. It creates an variable called element that gets a specific id from the DOM.
+    //Then it listens for a click and returns the variable element.
 function onClick(id, action) {
     let element = document.getElementById(id);
     element.addEventListener('click', action);
     return element;
 }
 
+//This function just returns the value of whatever is in the input field for a specific id.
 function getValue(id) {
     return document.getElementById(id).value;
 }
 
+//This function targets the div that holds the Trainer tables. It activates createTrainerTable function. 
+    //It also activates the createPokemonRow function.
 function drawDOM() {
     let trainerDiv = document.getElementById('trainers');
     clearElement(trainerDiv);
@@ -52,6 +62,7 @@ function drawDOM() {
     }
 }
 
+//This function fleshes out how the Pokemon rows on the table are structured.
 function createPokemonRow(trainer, table, pokemon) {
     let row = table.insertRow(2);
     row.insertCell(0).innerHTML = pokemon.name;
@@ -59,6 +70,7 @@ function createPokemonRow(trainer, table, pokemon) {
     row.insertCell(2).innerHTML = pokemon.type;
 }
 
+//This function creates the button on the trainer's table that allows the user to add a Pokemon row to the table.
 function createNewPokemonButton(trainer) {
     let btn = document.createElement('button');
     btn.className = 'btn btn-success';
@@ -70,6 +82,10 @@ function createNewPokemonButton(trainer) {
     return btn;
 }
 
+//This array holds the various types of Pokemon.
+const types = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water', 'Mixed'];
+
+//This function creates the entire Trainer table and dictates how it is structured and how it is designed.
 function createTrainerTable(trainer) {
     let table = document.createElement('table');
     table.setAttribute('class', 'table table-dark table-striped');
@@ -96,10 +112,16 @@ function createTrainerTable(trainer) {
     levelInput.setAttribute('id', `level-input-${trainer.id}`);
     levelInput.setAttribute('type', 'text');
     levelInput.setAttribute('class', 'form-control');
-    let typeInput = document.createElement('input');
+    let typeInput = document.createElement('select');
     typeInput.setAttribute('id', `type-input-${trainer.id}`);
-    typeInput.setAttribute('type', 'text');
+    typeInput.setAttribute('type', 'option');
     typeInput.setAttribute('class', 'form-control');
+    for(const i of types) {
+        let option = document.createElement('option');
+        option.type = i;
+        option.text = i.charAt(0).toUpperCase() + i.slice(1);
+        typeInput.appendChild(option); 
+    };
     let newPokemonButton = createNewPokemonButton(trainer);
     nameTh.appendChild(nameInput);
     levelTh.appendChild(levelInput);
@@ -112,6 +134,8 @@ function createTrainerTable(trainer) {
     return table;
 }
 
+//This function clears the previous trainer from the element. This will prevent duplicate trainers from appearing 
+    //as the user adds additional trainers.
 function clearElement(element) {
     while(element.firstChild) {
         element.removeChild(element.firstChild);
